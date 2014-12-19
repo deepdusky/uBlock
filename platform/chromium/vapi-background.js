11c563da7064fd003760d60cb52c0543b7e50771
Deathamns <deathamns@gmail.com>
2014-12-19 13:17:25 +0100

    Chrome,Safari: fix typo and quotes

--- platform/chromium/vapi-background.js
+++ platform/chromium/vapi-background.js
@@ -216,7 +216,7 @@ vAPI.tabs.injectScript = function(tabId, details, callback) {
         }
     };
     if ( tabId ) {
-        tabid = parseInt(tabId, 10);
+        tabId = parseInt(tabId, 10);
         chrome.tabs.executeScript(tabId, details, onScriptExecuted);
     } else {
         chrome.tabs.executeScript(details, onScriptExecuted);
